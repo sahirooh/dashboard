a87cca21b26872ba1607ffd22f2faa0d10a69f0f
sahirooh <sahirooh@gmail.com>
2023-10-26 23:56:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-center text-red-500">This is tailwindCSS</h1>
+        <Navbar></Navbar>
+      </div>
+    </div>
+  );
+};
+
+export default App;
